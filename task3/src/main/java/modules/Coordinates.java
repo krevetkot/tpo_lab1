7755588784dec8d0000f191d564243b951e22133
krevetkot <107612419+krevetkot@users.noreply.github.com>
2026-04-03 00:00:31 +0300

    delete excess methods

--- task3/src/main/java/modules/Coordinates.java
+++ task3/src/main/java/modules/Coordinates.java
@@ -5,26 +5,9 @@ public class Coordinates {
     private double y;
     private double z;
 
-    public Coordinates(){
-        x = 0;
-        y = 0;
-        z = 0;
-    }
     public Coordinates(double x, double y, double z){
         this.x = x;
         this.y = y;
-        this.z = z;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
-    public void setZ(double z) {
         this.z = z;
     }
 
